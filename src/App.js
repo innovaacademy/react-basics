7111c87d749e0a6edf180d3aea3b419b610736ab
Wissem Dhaouadi <wissem.dhaouadi@groupechantelle.com>
2020-05-12 14:44:05 +0200

    update person styles

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 
 import Person from './Person/Person';
 
 import './App.css';
 
 function App() {
-  const [persons, setPersons] = useState([{name: "Aymen", age: "32"},{name: "Sana", age: "18"},{name: "Hanen", age: "25"}])
-  
-  const handleButtonClick = () => {
-    setPersons(
-      [
-        {name: "Hamza", age: "23"},
-        {name: "Khaled", age: "14"},
-        {name: "Habib", age: "25"}
-      ]
-    )
-  }
+  const [persons, setPersons] = useState([])
+  const [fetchPerson, setFetchPerson] = useState(false);
 
-  const changeName = (event) => {
-    setPersons(
-      [
-        {name: "Hamza", age: "23"},
-        {name: "Khaled", age: "14"},
-        {name: event.target.value, age: "25"}
-      ]
-    )
-  }
+  useEffect( () => {
+
+    const fetchData = async () => {
+      const results = await axios.get('https://reqres.in/api/users')
+      setPersons(results.data.data)
+    }
+
+    if (fetchPerson === true ) {
+      fetchData();
+    }
+    
+
+  }, [ fetchPerson ])
+
+
   return (
        <div className="App">
          <h2>This is our App</h2>
-         <button onClick={handleButtonClick}>Switch Names</button>
+         <button onClick={() => setFetchPerson(true)}>Fetch Persons</button>
 
-         {
-           persons.map((person, index) => (
-              <Person key={index} name={person.name} age={person.age} changeName={changeName} />
-           ))
-         }
-
+        {
+          persons.map( (item, index) => {   
+            return <Person key={index} item={item} />
+          })
+        }
       </div>
         
   );
